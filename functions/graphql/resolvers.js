aec272ffc55e6985cc0698c9004c473f81b6f2cb
Tieu Luu <tieuluu@MacBook-Pro.fios-router.home>
2019-05-11 23:05:48 -0400

    got Query working with firestore instead of realtime database

--- functions/graphql/resolvers.js
+++ functions/graphql/resolvers.js
@@ -1,18 +1,45 @@
+const firebase = require('firebase');
 const admin = require('firebase-admin');
 const functions = require('firebase-functions');
 
 admin.initializeApp(functions.config().firebase);
 const foldersRef = admin.database().ref('folders');
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyAb1J8fK0uSwf-zz95h8G0Mu1fD9gT70_k",
+  authDomain: "graphql-sample.firebaseapp.com",
+  databaseURL: "https://graphql-sample.firebaseio.com",
+  projectId: "graphql-sample",
+  storageBucket: "graphql-sample.appspot.com",
+  messagingSenderId: "233011101239",
+  appId: "1:233011101239:web:ad02a29ac7bb5e3d"
+};
+const app = firebase.initializeApp(config);
+const db = firebase.firestore(app);
+
 module.exports = {
   Query: {
     folders() {
-      return foldersRef.once('value')
-        .then(snapshot => {
-          const folders = snapshot.val();
-          if (folders === null) return [];
-          return Object.keys(folders).map(o => Object.assign({ id: o }, folders[o]));
-        });
-    },
+      console.log("folders() ");
+      var collectionRef = db.collection("folders");
+      return collectionRef.get().then((results) => {
+        console.log("RESULTS size: " + results.size);
+
+        if (results.size > 0) {
+          let folders = [];
+
+          return results.docs.map(doc => {
+              let target = Object.assign({id: doc.id}, {name: doc.data().name});
+              console.log(target);
+              return target;
+          });
+        }
+        else {
+          return [];
+        }
+      });
+    }
   },
   Mutation: {
     createFolder(_, { input }) {
